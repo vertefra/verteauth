c2babeddbcb978093a8defe7b43b144e12f3102c
vertefra <verte.fra@gmail.com>
2020-11-11 11:23:09 -0500

    pre dep

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path'
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
 app.use(express.static('/build'))
 
 app.get('*', (req, res) => {
-  res.render('/index.html')
+  res.sendFile(_dirname + '/build/index.html')
 })
 
-app.listen(3001, () => {
-  console.log('server listening')
+app.listen(PORT, () => {
+  console.log('server listening: ', PORT)
 })
